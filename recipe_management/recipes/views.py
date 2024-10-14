6df811c5c69a661197540f864e379d4e8114f8a8
jamnjeri <jamnjeri@gmail.com>
2024-10-14 17:10:51 +0300

    token issues

--- recipe_management/recipes/views.py
+++ recipe_management/recipes/views.py
@@ -6,6 +6,7 @@ from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from .permissions import IsRecipeOwner
+from django.contrib.auth import authenticate
 
 # Create your views here.
 class RecipeViewSet(viewsets.ModelViewSet):
